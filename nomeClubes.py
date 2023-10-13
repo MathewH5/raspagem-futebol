ed6b54ea679767bcdcc057e74c0794adaa14cc26
Mathew <matheoshanna10@gmail.com>
2023-10-13 03:19:00 -0300

    achar o nome de todos os times da liga, e automatizar a procura do historico de todos os times sozinho

---
+++ nomeClubes.py
@@ -0,0 +1,86 @@
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import time
+from selenium.webdriver.chrome.options import Options
+
+ligas = ["Arabia saudita", "campeonato Argentino", "Eredivisie", "Liga NOS", "Ligue 1", "Campeonato Brasileiro", "premier-league", "Bundesliga", "Serie A", "La Liga",]  # Adicione mais nomes conforme necessário
+
+for liga in ligas:
+
+    driver = webdriver.Chrome()
+
+    # Abrir a página da web
+    driver.get('https://footystats.org/pt/saudi-arabia/professional-league')
+
+    botao_fechar = driver.find_element(By.CSS_SELECTOR, 'img[id="clever_40359_close_btn"]')
+    if botao_fechar:
+        botao_fechar.click()
+
+    #elemento = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Procurar Equipas e Ligas"]')
+    #elemento.send_keys(liga)  # Usar o nome da lista
+    #time_prcourado = liga
+    #elemento.send_keys(Keys.RETURN)
+
+    time.sleep(2)
+
+    #primeira_opcao = driver.find_element(By.CSS_SELECTOR, 'ul > li a.cf')
+    #primeira_opcao.click()
+
+    #time.sleep(3)
+
+    page_source = driver.page_source
+
+    site = BeautifulSoup(page_source, 'html.parser')
+
+    dic_time = {'time':[], 'vitoria':[], 'empates':[], 'derrotas':[], 'golsFeitos':[], 'golsTomados':[], 'over1.5':[], 'over2.5':[],'mediaGols':[]}
+
+    tabela = site.find('div', attrs={'class': 'table-wrapper'})
+    tbody = tabela.find('tbody')
+    time = tbody.findAll('tr')
+
+
+    print(time)
+    for jogo in time:
+        #nome = jogo.find('td', attrs={'class': 'team borderRightContent'}).find('a').get_text().strip()
+        #nome = jogo.find_element(By.CSS_SELECTOR, 'td.team > a.bold').text.strip()
+        #vitoria = jogo.find_element(By.CSS_SELECTOR, 'td.win').get_text().strip()
+        #empates = jogo.find_element(By.CSS_SELECTOR, 'td.draw').get_text().strip()
+        #derrotas = jogo.find_element(By.CSS_SELECTOR, 'td.loss').get_text().strip()
+        #gf = jogo.find_element(By.CSS_SELECTOR, 'td.gf').get_text().strip()
+        #gc = jogo.find_element(By.CSS_SELECTOR, 'td.ga').get_text().strip()
+        #over1_5 = jogo.find_element(By.CSS_SELECTOR, 'td.over15').get_text().strip()
+        #over2_5 = jogo.find_element(By.CSS_SELECTOR, 'td.over25').get_text().strip()
+        #mediaGols = jogo.find_element(By.CSS_SELECTOR, 'td.avg').get_text().strip()
+
+        nome = jogo.find('td', class_='team borderRightContent').find('a', class_='bold').text
+        vitorias = jogo.find('td', class_='win').text
+        empates = jogo.find('td', class_='draw').text
+        derrotas = jogo.find('td', class_='loss').text
+        gf = jogo.find('td', class_='gf').text
+        gc = jogo.find('td', class_='ga').text
+        over1_5 = jogo.find('td', class_='over15').text
+        over2_5 = jogo.find('td', class_='over25').text
+        mediaGols = jogo.find('td', class_='avg').text
+
+        dic_time['time'].append(nome)
+        dic_time['vitoria'].append(vitorias)
+        dic_time['empates'].append(empates)
+        dic_time['derrotas'].append(derrotas)
+        dic_time['golsFeitos'].append(gf)
+        dic_time['golsTomados'].append(gc)
+        dic_time['over1.5'].append(over1_5)
+        dic_time['over2.5'].append(over2_5)
+        dic_time['mediaGols'].append(mediaGols)
+
+        df = pd.DataFrame(dic_time)
+        # Suponhamos que você tenha um valor para 'elemento.send_keys' como 'cruzeiro'
+        nome_do_arquivo = f'C:/Users/mathe/Downloads/{liga}_historico.csv'
+        # Em seguida, use o nome_do_arquivo na chamada df.to_csv
+        df.to_csv(nome_do_arquivo, encoding='utf-8-sig', sep=',', index=False)
+
+
